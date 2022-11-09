250a6e22421132db906c780f2b738c8058d214ca
HuynhHuongNguyen <huong.nh@drd-x.com>
2022-11-09 09:42:44 +0700

    change test1

--- src/test/java/demo/StepDefinitions.java
+++ src/test/java/demo/StepDefinitions.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
